7aad033bcff1244ab7c82b3c53c71ce9a2d3dd5c
Vitalii Slobodoniuk <slvitalik24@ukr.net>
2022-01-26 12:19:37 +0200

    task done

--- script.js
+++ script.js
@@ -1,211 +1,116 @@
-const input = document.querySelector('.todo-input');
-const createButton = document.querySelector('#create-todo');
-let list = document.getElementById('list');
-
+const input = $('.todo-input');
+const createButton = $('#create-todo');
 const requestURL = 'http://localhost:3000/todos';
-
-function getJSON(url) {
-  return new Promise(function (resolve, reject) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('get', url, true);
-
-    xhr.responseType = 'json'; // ответ будет в виде обьекта, а не строка
-
-    xhr.onload = function () {
-      let status = xhr.status;
-
-      if (status === 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-
-    xhr.send();
-  });
-}
-
-const postJSON = function (url, data) {
-  return new Promise(function (resolve, reject) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('post', url, true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-    xhr.responseType = 'json';
-
-    xhr.onload = function () {
-      let status = xhr.status;
-
-      if (status === 201) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-
-    xhr.onerror = function (e) {
-      reject('Error fetching ' + url);
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-};
-
-const putJSON = function (url, data) {
-  return new Promise(function (resolve, reject) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('put', url, true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-    xhr.responseType = 'json';
-
-    xhr.onload = function () {
-      let status = xhr.status;
-
-      if (status === 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-
-    xhr.onerror = function (e) {
-      reject('Error fetching ' + url);
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-};
-
-const deleteJSON = function (url) {
-  return new Promise(function (resolve, reject) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('delete', url, true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-    xhr.responseType = 'json';
-
-    xhr.onload = function () {
-      resolve(xhr.response);
-    };
-
-    xhr.onerror = function (e) {
-      reject('Error fetching ' + url);
-    };
-
-    xhr.send(null);
-  });
-};
+let list = $('#list');
 
 class TodoList {
-  constructor(elem) {
+  constructor(elem, url) {
     this.elem = elem;
-    this.elem.addEventListener('click', (event) => {
-      let target = event.target;
-      let todoId = target.closest('li').dataset.id;
+    this.url = url;
+  }
 
-      if (target.className.includes('set-status')) {
-        newTodo.changeTodoStatus(todoId);
-      } else if (target.className.includes('delete-task')) {
-        newTodo.removeTodo(todoId);
-      }
+  addTodo(todo) {
+    $.ajax({
+      type: 'post',
+      url: this.url,
+      data: JSON.stringify({
+        task: todo,
+        complited: false,
+      }),
+      headers: { 'Content-type': 'application/json; charset=utf-8' },
+    });
+    // this.showTodoList();
+    location.reload();
+  }
+
+  removeTodo(id) {
+    $.ajax({
+      type: 'delete',
+      url: `${this.url}/${id}`,
+      headers: { 'Content-type': 'application/json' },
+      data: null,
     });
   }
 
-  async getDataFromDB() {
+  getDataFromDB() {
+    return $.ajax({
+      url: this.url,
+      dataType: 'json',
+      success: () => {
+        console.log('Operation success');
+      },
+    });
+  }
+
+  async showTodoList() {
     try {
-      let data = await getJSON(requestURL);
-      return data;
+      let data = await this.getDataFromDB();
+      this.render(data);
     } catch (error) {
-      console.log(new Error(error));
+      console.log('Что-то пошло не так! Ошибочка: ', error);
     }
   }
 
-  async addTodo() {
-    try {
-      if (input.value !== '') {
-        await postJSON(requestURL, {
-          task: input.value,
-          complited: false,
-        });
-        this.render();
+  render(data) {
+    let res = '';
+
+    for (let el of data) {
+      //   console.log(data);
+      //   console.log(el);
+      if (!el) {
+        return;
       }
-    } catch (error) {
-      console.log(error);
+      res += `<li class="list-item ${el.complited ? 'done' : 'must-done'}" data-id="${el.id}">${
+        el.task
+      }
+          <button class="set-status">Change status</button><button class="delete-task">Delete</button></li>`;
     }
+    // console.log(res);
+    this.elem.html(res);
   }
 
   async changeTodoStatus(id) {
     try {
-      let data = await getJSON(requestURL);
+      let data = await this.getDataFromDB();
 
       for (let item of data) {
         if (item.id == id) {
           item.complited = !item.complited;
-          let todoToChangeStatus = document.querySelector(`[data-id="${id}"]`);
 
-          this.changeTodoColor(todoToChangeStatus);
-
-          putJSON(`${requestURL}/${id}`, {
-            task: item.task,
-            complited: item.complited,
+          $.ajax({
+            type: 'put',
+            url: `${this.url}/${id}`,
+            data: JSON.stringify({
+              task: item.task,
+              complited: item.complited,
+            }),
+            headers: { 'Content-type': 'application/json' },
           });
         }
       }
+      location.reload();
     } catch (error) {
-      console.log(new Error(error));
-    }
-  }
-
-  changeTodoColor(el) {
-    el.classList.toggle('done');
-  }
-
-  async removeTodo(id) {
-    try {
-      let data = await getJSON(requestURL);
-
-      for (let item of data) {
-        if (item.id == id) {
-          deleteJSON(`${requestURL}/${id}`);
-        }
-        this.render();
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async render() {
-    let res = '';
-
-    try {
-      let data = await this.getDataFromDB();
-
-      for (let el of data) {
-        if (!el) {
-          return;
-        }
-        res += `<li class="list-item must-done ${el.complited ? 'done' : 'must-done'}" data-id="${
-          el.id
-        }">${el.task}
-          <button class="set-status">Change status</button><button class="delete-task">Delete</button></li>`;
-      }
-      this.elem.innerHTML = res;
-    } catch (error) {
-      console.log(new Error(error));
+      console.log('Что-то пошло не так! Ошибочка: ', error);
     }
   }
 }
 
-let newTodo = new TodoList(list);
-newTodo.render();
+let newTodo = new TodoList(list, requestURL);
+newTodo.showTodoList();
 
-createButton.addEventListener('click', function () {
-  newTodo.addTodo();
-  input.value = '';
+createButton.on('click', () => {
+  newTodo.addTodo(input.val());
+  input.val(null);
+});
+
+list.on('click', (e) => {
+  let target = e.target;
+  let todoId = target.closest('li').dataset.id;
+
+  if (target.className.includes('set-status')) {
+    newTodo.changeTodoStatus(todoId);
+  } else if (target.className.includes('delete-task')) {
+    newTodo.removeTodo(todoId);
+    location.reload();
+  }
 });
